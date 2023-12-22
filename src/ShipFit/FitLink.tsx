dc42732b0c0b4801befff90173f11217879b8d31
Patric Stout <github@truebrain.nl>
2023-12-22 20:28:52 +0100

    feat: "Share Link" button to quickly share the current fit

--- src/ShipFit/FitLink.tsx
+++ src/ShipFit/FitLink.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { useEveShipFitLink } from "../EveShipFitLink";
+import { useClipboard } from "../Helpers/Clipboard";
 
 import styles from "./ShipFit.module.css";
 
@@ -14,26 +15,6 @@ const useIsRemoteViewer = () => {
   }, []);
 
   return remote;
-}
-
-/* Based on https://github.com/mantinedev/mantine/blob/master/src/mantine-hooks/src/use-clipboard/use-clipboard.ts */
-export function useClipboard({ timeout = 2000 } = {}) {
-  const [copied, setCopied] = React.useState(false);
-  const [copyTimeout, setCopyTimeout] = React.useState<number | undefined>(undefined);
-
-  const handleCopyResult = (value: boolean) => {
-    if (copyTimeout !== undefined) {
-      window.clearTimeout(copyTimeout);
-    }
-    setCopyTimeout(window.setTimeout(() => setCopied(false), timeout));
-    setCopied(value);
-  };
-
-  const copy = (valueToCopy: string) => {
-    navigator.clipboard.writeText(valueToCopy).then(() => handleCopyResult(true));
-  };
-
-  return { copy, copied };
 }
 
 export const FitLink = () => {
