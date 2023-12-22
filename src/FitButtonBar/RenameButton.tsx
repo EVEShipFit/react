738c4783ab05190c14de42ff8a9a452ed7059509
Patric Stout <github@truebrain.nl>
2023-12-22 20:32:08 +0100

    chore: refactor FitButtonBar into several smaller components

---
+++ src/FitButtonBar/RenameButton.tsx
@@ -0,0 +1,41 @@
+import clsx from "clsx";
+import React from "react";
+
+import { ShipSnapshotContext } from "../ShipSnapshotProvider";
+import { ModalDialog } from "../ModalDialog";
+
+import styles from "./FitButtonBar.module.css";
+
+export const RenameButton = () => {
+  const shipSnapshot = React.useContext(ShipSnapshotContext);
+
+  const [isRenameOpen, setIsRenameOpen] = React.useState(false);
+  const [rename, setRename] = React.useState("");
+
+  const saveRename = React.useCallback(() => {
+    shipSnapshot?.setName(rename);
+    setIsRenameOpen(false);
+  }, [rename, shipSnapshot]);
+
+  const openRename = React.useCallback(() => {
+    setRename(shipSnapshot?.fit?.name ?? "");
+    setIsRenameOpen(true);
+  }, [shipSnapshot]);
+
+  return <>
+    <div className={styles.button} onClick={() => openRename()}>
+      Rename
+    </div>
+
+    <ModalDialog visible={isRenameOpen} onClose={() => setIsRenameOpen(false)} title="Fit Name">
+      <div>
+        <span className={styles.renameEdit}>
+          <input type="text" autoFocus value={rename} onChange={(e) => setRename(e.target.value)} />
+        </span>
+        <span className={clsx(styles.button, styles.buttonSmall)} onClick={() => saveRename()}>
+          Save
+        </span>
+      </div>
+    </ModalDialog>
+  </>
+}
